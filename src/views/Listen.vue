6330bf6448922e09a66ee822fbab8f89c17007af
Annie Du <crystalrain10@gmail.com>
2019-05-22 23:41:42 -0700

    merged to master

---
+++ src/views/Listen.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="listen">
+    <TuneNav/>
+  </div>
+</template>
+
+<script>
+import TuneNav from '@/components/tunenav.vue'
+
+export default {
+  name: 'listen',
+  components: {
+      TuneNav
+  }
+}
+</script>
+
+<style>
+
+</style>
